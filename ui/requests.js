3dd77a5c33ea9d9f83e92ec738df74afedb6032e
TheGreat-Chain <iuteleve78@gmail.com>
2023-02-09 11:50:59 +0100

    display server response

--- ui/requests.js
+++ ui/requests.js
@@ -1,22 +1,21 @@
-const forms = document.querySelectorAll('form');
+const form = document.querySelector('form');
 
-forms.forEach(form => {
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-        const textarea = form.querySelector('textarea');
-        const content = textarea.value;
+    const textarea = form.querySelector('textarea');
+    const content = textarea.value;
 
-        fetch('http://localhost:3000/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ content })
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            });
-    });
+    fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.querySelector('#server-response').textContent = data.message;
+        });
 });
